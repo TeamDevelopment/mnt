77203cc15a9abd752dbef230668e2864aa0b677b
EriLee <td.EriLee@gmail.com>
2014-07-28 16:32:19 +0800

    publish first file
    EriLee publish first file

---
+++ deleteNS.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------------------------
+######  Copyright 2013 FrameSoul. All rights reserved.
+######  SCRIPT:EriLee_studioenv.py
+######  AUTHOR:EriLee(td.EriLee@gmail.com)
+######  DATE:January 31, 2013
+######  VERSION:2013.1
+#-----------------------------------------------------------------
+"""
+Studio Environment module for setting up the studio environment
+
+This is a very basic module that will just ensure that the studio package
+is added to the PYTHONPATH so that other scripts or an interactive python
+session can easily access the studio code.
+"""
+
+
+import maya.cmds as cmds
+
+
+def deleteNS():
+    sel_namespace = cmds.ls(sl=True)[0]
+    NS = sel_namespace.rsplit(':',1)[0]
+    if not cmds.namespace(ex=NS):
+        raise Exception('Namespace "'+NS+'" does not exist!')
+    cmds.namespace(mv=[NS,':'],f=True)
+    cmds.namespace(rm=NS)
